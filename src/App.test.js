2581730e3839bcf3d557999a2ca37b53fd45d305
rahul oberoi <rahuloberoi71@gmail.com>
2021-09-07 13:37:05 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/ I was Changed/i);
   expect(linkElement).toBeInTheDocument();
 });
 
